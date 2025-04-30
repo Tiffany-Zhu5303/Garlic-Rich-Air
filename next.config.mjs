0ff2bf332f01aeec61a67e9709c1600f3fe2bc49
Tiffany Zhu <Tiffany2016d@gmail.com>
2025-04-30 00:01:54 -0400

    updating static pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
     output: 'export',
+    trailingSlash: true,
+    basePath: '/garlic-rich-air', 
+    assetPrefix: '/garlic-rich-air',
 }
-;
 
 export default nextConfig;
